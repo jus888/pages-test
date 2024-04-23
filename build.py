e580ae67e6f5f736cc5c7f7fd2f6e3b832847a31
jus888 <4376095+jus888@users.noreply.github.com>
2024-04-23 16:06:39 +0200

    Markdown meta

--- build.py
+++ build.py
@@ -6,15 +6,25 @@ import bs4
 from bs4 import BeautifulSoup
 
 with open("content.md", "r") as file:
-    md = file.read()
-    md_html = markdown.markdown(md)
+    text = file.read()
+    md = markdown.Markdown(extensions = ['meta'])    
+    md_html = md.convert(text)
+    meta = md.Meta
+    print(meta['items'])
 
 with open("index.html", "r+") as file:
     soup = BeautifulSoup(file, "html.parser")
     md_soup = BeautifulSoup(md_html, "html.parser")    
 
-    main = soup.find(string=lambda text: type(text) == bs4.element.Comment)
-    main.parent.append(md_soup)
+    comments = soup.find_all(string=lambda text: type(text) == bs4.element.Comment)
+    for comment in comments:
+        comment_string = comment.string.strip()
+
+        if comment_string == "Main":
+            comment.parent.append(md_soup)
+
+        elif comment_string == "Title":
+            comment.parent.append(f"{meta['title'][0]}")
 
     file.seek(0)
     file.truncate(0)
--- content.md
+++ content.md
@@ -1,3 +1,11 @@
+---
+title: Test
+items: a
+    b
+    c
+    d
+---
+
 # Test
 - This is markdown
 - That has been inserted into html!
